ea81c9d66a76c6acfb65d2c40377b7bfd9749e0e
Thu Than <than@uni.minerva.edu>
2025-11-01 15:18:55 -0700

    feat: added introduction and interactive snake game

---
+++ frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom';
+import './App.css';
+import LearnPage from './pages/learnPage';
+import PlayPage from './pages/playPage';
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  const handlePlayClick = () => {
+    navigate('/play');
+  };
+
+  const snakePixels = Array.from({ length: 20 }, (_, i) => {
+    const style = {
+      animation: `snakeMove ${20 + i * 2}s linear infinite`,
+      top: `${Math.random() * 100}vh`,
+      left: `${Math.random() * 100}vw`,
+    };
+    return <div key={i} className="snake-pixel" style={style} />;
+  });
+
+  return (
+    <>
+      {snakePixels}
+      <div>
+        <div className="typing-container">
+          <h1 className="typing-effect">tinker and play_</h1>
+        </div>
+        <h2>a hands-on adventure in teaching machines to play</h2>
+      </div>
+      <button onClick={handlePlayClick}>Play</button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/play" element={<PlayPage />} />
+        <Route path="/learn" element={<LearnPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/learnPage.jsx
@@ -0,0 +1,9 @@
+export default function LearnPage() {
+    return (
+        <div className="play-page">
+            <h1>can ai do better or as good as you?</h1>
+            <p>we can use an algorithm called <span className="highlight">reinforcement learning</span> to teach computers to play the game itself</p>
+            <button>continue</button>
+        </div>
+    );
+}
